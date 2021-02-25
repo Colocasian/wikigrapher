6db497011c129af4c1626580e5718fa327ec1736
Rishvic Pushpakaran <rishvic@gmail.com>
2021-02-25 12:13:09 +0530

    added logging, and necessary libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use log::info;
+
 fn main() {
+    log4rs::init_file("./config/log4rs.yaml", Default::default())
+        .expect("expected a log4rs config file");
     println!("Hello, world!");
+    info!("started execution of wikigrapher");
+
+    info!("completed execution of wikigrapher");
 }
